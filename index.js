d0341e960c5aee1c3bb00d43255b3aa988f0fe1c
DeemoHarlos <cces4415@gmail.com>
2019-01-11 22:40:06 +0800

    Use pixi instead, add a hero sprite

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-var canvas = $('canvas#main')
+const App          = PIXI.Application
+const load         = PIXI.loader
+const resource     = PIXI.loader.resources
+const Sprite       = PIXI.Sprite
+const TextureCache = PIXI.utils.TextureCache
+
+var app = new App({
+	antialias:   true,    // default: false
+	transparent: false, // default: false
+	autoResize:  true,
+	resolution:  1,      // default: 1
+})
+
 
 function updateCanvasSize() {
 	var screenWidth  = window.innerWidth
 	var screenHeight = window.innerHeight
-	canvas.attr('width' ,screenWidth )
-	canvas.attr('height',screenHeight)
+	app.renderer.resize(screenWidth,screenHeight)
 }
 
 updateCanvasSize()
 window.addEventListener('resize',updateCanvasSize)
 
-var ctx = canvas.getContext('2d');
+$('body>.wrap').apnd(app.view)
+
+PIXI.loader.add("HERO.png").load(()=>{
+	var hero = new Sprite(resource["HERO.png"].texture)
+	app.stage.addChild(hero)
+})
